976eea599728f1b308801b8489110dd2717c8d77
lucascampos04 <camposdlucasoli@gmail.com>
2023-11-13 19:43:02 -0300

    Query de triagem, consulta e pacientes funcionando, com algumas inconsistencia porem funcionando

--- main.sql
+++ main.sql
@@ -1,10 +1,14 @@
+-- Tabela sequencia
+
 create sequence auto_increment
     start with 1
     increment by 1
-    maxvalue 9999; -- Auto INcrement
+    maxvalue 9999; -- Auto Increment
+
+-- Criando tabelas
 
 create table cadastrapaciente (
-    ra int default auto_increment.nextval primary key,
+    ra int primary key,
     nome varchar(90) not null,
     rg char(10) not null unique,
     cpf char(14) not null unique, 
@@ -27,7 +31,7 @@ create table funcionariosConsultasTriagem(
     tipo varchar(50) not null,
     foreign key(id_consultaTriagem) references consultatriagem(id),
     foreign key(rf_funcionario) references cadastrarfuncionario(rf)
- );
+ ); -- tabela intermediario de funcionario e consulta e triagem
 
 create table cadastrarfuncionario (
     rf int default auto_increment.nextval primary key,
@@ -88,3 +92,103 @@ create table obitos(
     id_consultaTriagem int not null,
     foreign key(id_consultaTriagem) references consultatriagem(id)
 ); -- Registrar um obito
+
+
+-- SELECT's
+
+-- Query de triagem
+select id, ra_paciente, rf_enfermeiro, horario_triagem, data, alergias, febre, tosse, fadiga, nausea, vomitos, diarreia, calafrios, dorsnasarticulacoes, dificuldaderespiratoria, perdodopaladarouolfato, congestaonasal
+    from consultatriagem; -- busca de dados da triagem
+
+-- Query de consulta
+select id, ra_paciente, rf_medico, horario_consulta, horario_consulta_inicio,data, horario_consulta_termino, formadepagamento, valor_consulta 
+    from consultatriagem; -- busca de dados da consulta
+
+-- INSERT's
+
+-- marcando consulta e triagem
+INSERT INTO consultatriagem (id, ra_paciente, tipoconsulta, 
+    alergias, febre, tosse, fadiga, nausea, vomitos, diarreia, calafrios, dorsnasarticulacoes, dificuldaderespiratoria, perdodopaladarouolfato, congestaonasal, 
+    rf_enfermeiro, horario_triagem, 
+    data, 
+    rf_medico, 
+    horario_consulta, horario_consulta_inicio, horario_consulta_termino, 
+    valor_consulta, formadepagamento)
+VALUES (2,
+    	47, 
+    	'Triagem',
+    	'Sim', 'Não', 'Sim', 'Não', 'Não', 'Não', 'Não', 'Sim', 'Não', 'Não', 'Sim', 'Não',
+    	44, '08:00',
+    	to_date('2023-11-13', 'YYYY-MM-DD'),
+    	45,
+    	'10:00', '10:30', '11:00',
+    	50.00, 'Dinheiro'); -- marca consulta e triagem
+
+-- cadastrar enfermeiro
+INSERT INTO cadastrarfuncionario (nome, rg, cpf, genero, endereco, rua, bairro, estado, cidade, cep, numero, complemento, email, setor, turno, salario)
+VALUES ('Enfermeiro Exemplo',
+    '9876543210',
+    '987.654.321-01',
+    'M',
+    'Endereço do Enfermeiro',
+    'Rua do Enfermeiro',
+    'Bairro do Enfermeiro',
+    'Estado do Enfermeiro',
+    'Cidade do Enfermeiro',
+    '87654321',
+    '987',
+    'Complemento do Enfermeiro',
+    'enfermeiro@example.com',
+    'Triagem',
+    'Diurno',
+    5000.00); -- enfermeiro 
+
+-- cadastrar medico
+INSERT INTO cadastrarFuncionario (nome, rg, cpf, genero, endereco, rua, bairro, estado, cidade, cep, numero, complemento, email, setor, turno, salario)
+VALUES ('Dr. João Silva', 
+    	'1234567890', 
+    	'123.456.789-01', 
+    	'M', 
+    	'Rua Hospitalar',
+    	'Avenida da Saúde',
+    	'Bairro da Cura',
+    	'Estado da Medicina', 
+    	'Cidade da Saúde', 
+    	'12345678', 
+    	'123', 
+    	'Próximo à Emergência', 
+    	'drjoao@example.com',
+    	'Consulta',
+    	'Diurno',
+    	9000.00); -- medico
+
+
+-- Cadastrar paciente
+INSERT INTO cadastrapaciente (ra, nome, rg, cpf, genero, endereco, rua, bairro, estado, cidade, cep, numero, complemento, formadepagamento, email)
+VALUES (auto_increment.nextval, 
+    	'Nome do Paciente', 
+    	'1234562890', 
+    	'223.456.789-01', 
+    	'M', 
+    	'Endereço do Paciente', 	
+   	 	'Rua do Paciente', 
+    	'Bairro do Paciente', 
+    	'Estado do Paciente', 
+    	'Cidade do Paciente', 
+    	'12345678', 
+    	'123', 
+    	'Complemento do Paciente', 
+    	'Forma de Pagamento do Paciente', 
+    	'paciente@example.com'); -- paciente
+
+-- ALTER TABLE's
+ALTER TABLE consultatriagem modify febre char(4);
+ALTER TABLE consultatriagem modify fadiga char(4);
+ALTER TABLE consultatriagem modify nausea char(4);
+ALTER TABLE consultatriagem modify vomitos char(4);
+ALTER TABLE consultatriagem modify diarreia char(4);
+ALTER TABLE consultatriagem modify calafrios char(4);
+ALTER TABLE consultatriagem modify dorsnasarticulacoes char(4);
+ALTER TABLE consultatriagem modify dificuldaderespiratoria char(4);
+ALTER TABLE consultatriagem modify perdodopaladarouolfato char(4);
+ALTER TABLE consultatriagem modify congestaonasal char(4);
