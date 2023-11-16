193b7a985785ba9ad3794ea46557f15531379fc9
lucascampos04 <camposdlucasoli@gmail.com>
2023-11-16 17:12:40 -0300

    com consultas atualizadas

--- main.sql
+++ main.sql
@@ -4,12 +4,12 @@ create sequence auto_increment
     increment by 1
     maxvalue 9999;
 
--- Cadastrar Paciente 
+-- Cadastrar Paciente
 create table cadastrarpaciente (
     ra int primary key,
     nome varchar(90) not null,
     rg char(10) not null unique,
-    cpf char(14) not null unique, 
+    cpf char(14) not null unique,
     genero char(1) not null,
     rua varchar(20) not null,
     bairro varchar(30) not null,
@@ -28,7 +28,7 @@ create table cadastrarfuncionario (
     rf int primary key,
     nome varchar(90) not null,
     rg char(10) not null unique,
-    cpf char(14) not null unique, 
+    cpf char(14) not null unique,
     genero char(1) not null,
     rua varchar(150) not null,
     bairro varchar(30) not null,
@@ -48,13 +48,13 @@ create table cadastrarfuncionario (
 );
 
 -- Tabela intermediaria de funcionario e triagem / consulta
-    create table funcionariosConsultasTriagem (
-        id_consultaTriagem int not null,
-        rf_funcionario int not null,
-        tipo varchar(50) not null,
-        foreign key (id_consultaTriagem) references consultatriagem (id),
-        foreign key (rf_funcionario) references cadastrarfuncionario (rf)
-    );
+create table funcionariosConsultasTriagem (
+    id_consultaTriagem int not null,
+    rf_funcionario int not null,
+    tipo varchar(50) not null,
+    foreign key (id_consultaTriagem) references consultatriagem (id),
+    foreign key (rf_funcionario) references cadastrarfuncionario (rf)
+);
 
 -- Marca consulta ou triagem
 CREATE TABLE consultatriagem (
@@ -85,8 +85,9 @@ CREATE TABLE consultatriagem (
     condicoes_pre_existentes varchar(25),
     medicacoes varchar(25),
     observacoes_adicionais varchar(50),
-    status VARCHAR(10) DEFAULT 'Realizado',
-    CHECK((horario_consulta_inicio IS NOT NULL AND horario_consulta_termino IS NOT NULL) OR (horario_consulta_inicio IS NULL AND status = 'Cancelado')),
+    situacao VARCHAR(10) DEFAULT 'Realizado',
+    CHECK((horario_consulta_inicio IS NOT NULL AND horario_consulta_termino IS NOT NULL) 
+    OR (horario_consulta_inicio IS NULL AND status = 'Cancelado')),
     FOREIGN KEY (ra_paciente) REFERENCES cadastrarpaciente(ra),
     FOREIGN KEY (rf_medico) REFERENCES cadastrarfuncionario(rf),
     FOREIGN KEY (rf_enfermeiro) REFERENCES cadastrarfuncionario(rf)
@@ -151,6 +152,42 @@ INSERT INTO cadastrarpaciente (
     auto_increment.nextval, 'Ana Oliveira', 789012345, 12398765432, 'F', 'Rua D', 'Copacabana', 'RJ', 'Rio de Janeiro', 06789012, 101, 'Casa 15', 'ana@email.com', 11901234567, to_date('1992-05-18', 'YYYY-MM-DD')
 );
 
+INSERT INTO cadastrarpaciente (
+    ra, nome, rg, cpf, genero,
+    rua, bairro, estado, cidade, cep, numero, complemento,
+    email, telefone, data_nascimento
+) VALUES (
+    auto_increment.nextval, 'Gabriel Lima', 123426789, 44321098765, 'M', 'Rua Q', 'Morumbi', 'SP', 'São Paulo', 03210987, 678, 'Apto 401', 'gabriel@email.com', 11876543210, to_date('1988-06-15', 'YYYY-MM-DD')
+);
+
+-- Paciente 18
+INSERT INTO cadastrarpaciente (
+    ra, nome, rg, cpf, genero,
+    rua, bairro, estado, cidade, cep, numero, complemento,
+    email, telefone, data_nascimento
+) VALUES (
+    auto_increment.nextval, 'Aline Oliveira', 234567830, 31109876543, 'F', 'Rua R', 'Campo Limpo', 'SP', 'São Paulo', 09876543, 901, 'Casa 18', 'aline@email.com', 11901234567, to_date('1990-01-20', 'YYYY-MM-DD')
+);
+
+-- Paciente 19
+INSERT INTO cadastrarpaciente (
+    ra, nome, rg, cpf, genero,
+    rua, bairro, estado, cidade, cep, numero, complemento,
+    email, telefone, data_nascimento
+) VALUES (
+    auto_increment.nextval, 'Pedro Santos', 563890123, 43210987154, 'M', 'Rua S', 'Vila Leopoldina', 'SP', 'São Paulo', 07654321, 234, 'Apto 501', 'pedro@email.com', 11987654321, to_date('1985-03-18', 'YYYY-MM-DD')
+);
+
+-- Paciente 20
+INSERT INTO cadastrarpaciente (
+    ra, nome, rg, cpf, genero,
+    rua, bairro, estado, cidade, cep, numero, complemento,
+    email, telefone, data_nascimento
+) VALUES (
+    auto_increment.nextval, 'Mariana Oliveira', 6789012134,11098765432, 'F', 'Rua T', 'Santana', 'SP', 'São Paulo', 08765432, 345, 'Casa 16', 'mariana@email.com', 11876543210, to_date('1992-09-10', 'YYYY-MM-DD')
+);
+
+
 -- cadastrar funcionario
 
 INSERT INTO cadastrarfuncionario (
@@ -211,6 +248,7 @@ INSERT INTO cadastrarfuncionario (
 
 select rf, cargo from cadastrarfuncionario;
 select ra from cadastrarpaciente;
+select id from consultatriagem;
 
 -- consulta / triagem
 INSERT INTO consultatriagem (
@@ -223,8 +261,8 @@ INSERT INTO consultatriagem (
     observacoes_adicionais
 ) VALUES (
     auto_increment.nextval, 1, 'triagem', 'Não', 'Não', 'Não', 'Não', 'Não',
-    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 7, '08:30',
-    to_date('2023-11-17', 'YYYY/MM/DD'), 6, '09:00', '09:15', '09:30', 'Normal',
+    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 37, '08:30',
+    to_date('2023-11-17', 'YYYY/MM/DD'), 34, '09:00', '09:15', '09:30', 'Normal',
     'Nenhum', 'Nenhuma', 'Nenhuma', 'Nenhuma'
 );
 
@@ -237,9 +275,9 @@ INSERT INTO consultatriagem (
     sintomas_especificos, condicoes_pre_existentes, medicacoes,
     observacoes_adicionais
 ) VALUES (
-    auto_increment.nextval 1, 'Consulta', 'Não', 'Não', 'Não', 'Não', 'Não',
-    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 8, '10:45',
-    to_date('2023-11-17', 'YYYY/MM/DD'), 9, '11:30', '11:45', '12:00', 'Urgente',
+    auto_increment.nextval, 1, 'Consulta', 'Não', 'Não', 'Não', 'Não', 'Não',
+    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 35, '10:45',
+    to_date('2023-11-17', 'YYYY/MM/DD'), 38, '11:30', '11:45', '12:00', 'Urgente',
     'Dor no peito', 'Hipertensão', 'Aspirina', 'Paciente relatou dor intensa no peito.'
 );
 
@@ -253,8 +291,8 @@ INSERT INTO consultatriagem (
     observacoes_adicionais
 ) VALUES (
     auto_increment.nextval, 3, 'Consulta', 'Não', 'Sim', 'Sim', 'Não', 'Sim',
-    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 9, '14:00',
-    to_date('2023-11-17', 'YYYY/MM/DD'), 10, '14:30', '14:45', '15:00', 'Normal',
+    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 35, '14:00',
+    to_date('2023-11-17', 'YYYY/MM/DD'), 34, '14:30', '14:45', '15:00', 'Normal',
     'Febre e tosse', 'Nenhuma', 'Tylenol', 'Criança com sintomas gripais.'
 );
 
@@ -268,8 +306,8 @@ INSERT INTO consultatriagem (
     observacoes_adicionais
 ) VALUES (
     auto_increment.nextval, 4, 'Consulta', 'Não', 'Não', 'Não', 'Não', 'Não',
-    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 7, '16:30',
-    to_date('2023-11-17', 'YYYY/MM/DD'), 10, '17:15', '17:30', '17:45', 'Normal',
+    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 37, '16:30',
+    to_date('2023-11-17', 'YYYY/MM/DD'), 38, '17:15', '17:30', '17:45', 'Normal',
     'Nenhum', 'Nenhuma', 'Nenhuma', 'Exame de rotina.'
 );
 
@@ -284,53 +322,126 @@ INSERT INTO consultatriagem (
     observacoes_adicionais, status
 ) VALUES (
     auto_increment.nextval, 1, 'Consulta', 'Não', 'Não', 'Não', 'Não', 'Não',
-    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 7, '09:00',
-    to_date('2023-11-17', 'YYYY/MM/DD'), 10, '09:45', 'Normal',
+    'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 'Não', 37, '09:00',
+    to_date('2023-11-17', 'YYYY/MM/DD'), 36, '09:45', 'Normal',
     'Visão embaçada', 'Nenhuma', 'Nenhuma', 'Exame de vista de rotina.', 'Cancelado'
 );
 
 -- insert de obito
+INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
+VALUES (auto_increment.nextval, 'Parada cardíaca', 39, '15:30', TO_DATE('2023-11-17', 'YYYY/MM/DD'));
 
 INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
-VALUES (auto_increment.nextval, 'Parada cardíaca', 25, '15:30', TO_DATE('2023-11-17', 'YYYY/MM/DD'));
+VALUES (auto_increment.nextval, 'Insuficiência respiratória', 40, '18:45', TO_DATE('2023-11-18', 'YYYY/MM/DD'));
 
 INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
-VALUES (auto_increment.nextval, 'Insuficiência respiratória', 26, '18:45', TO_DATE('2023-11-18', 'YYYY/MM/DD'));
+VALUES (auto_increment.nextval, 'Acidente vascular cerebral', 60, '12:15', TO_DATE('2023-11-19', 'YYYY/MM/DD'));
 
 INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
-VALUES (auto_increment.nextval, 'Acidente vascular cerebral', 22, '12:15', TO_DATE('2023-11-19', 'YYYY/MM/DD'));
+VALUES (auto_increment.nextval, 'Septicemia', 61, '21:10', TO_DATE('2023-11-20', 'YYYY/MM/DD'));
 
 INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
-VALUES (auto_increment.nextval, 'Septicemia', 23, '21:10', TO_DATE('2023-11-20', 'YYYY/MM/DD'));
+VALUES (auto_increment.nextval, 'Falência múltipla de órgãos', 62, '09:00', TO_DATE('2023-11-21', 'YYYY/MM/DD'));
 
-INSERT INTO obitos (id, causaDaMorte, id_consultaOrTriagem, horaDoObito, data)
-VALUES (auto_increment.nextval, 'Falência múltipla de órgãos', 21, '09:00', TO_DATE('2023-11-21', 'YYYY/MM/DD'));
-
+-- Inserções na tabela funcionariosConsultasTriagem
+INSERT INTO funcionariosConsultasTriagem (id_consultaTriagem, rf_funcionario, tipo) VALUES (39, 36, 'Triagem');
+INSERT INTO funcionariosConsultasTriagem (id_consultaTriagem, rf_funcionario, tipo) VALUES (62, 37, 'Consulta');
+INSERT INTO funcionariosConsultasTriagem (id_consultaTriagem, rf_funcionario, tipo) VALUES (60, 34, 'Triagem');
+INSERT INTO funcionariosConsultasTriagem (id_consultaTriagem, rf_funcionario, tipo) VALUES (40, 35, 'Consulta');
 
 -- SELECTS 
+-- Consulta de Tipos e Quantidade de Triagens
+SELECT tipo, COUNT(*) as total_triagens
+FROM funcionariosConsultasTriagem
+GROUP BY tipo;
 
--- select checar consultas/pacientes 
-select id, ra_paciente, data, horario_consulta, horario_consulta_inicio, horario_consulta_termino, status ,tipoconsulta  from consultatriagem where tipoconsulta = 'Consulta';
+-- Consulta de Média Salarial por Cargo
+SELECT cargo, AVG(salario) as media_salarial
+FROM cadastrarfuncionario
+GROUP BY cargo;
 
--- select checar triagem 
-select id, ra_paciente, data, horario_triagem, alergias, febre, tosse, nausea, vomitos, diarreia, calafrios, dorsnasarticulacoes, dificuldaderespiratoria, perdodopaladarouolfato, congestaonasal from consultatriagem  where tipoconsulta = 'triagem';
+-- Consulta de Média de Mortes por Doença
+SELECT causaDaMorte, COUNT(*) as total_mortes
+FROM obitos
+GROUP BY causaDaMorte;
 
--- select de obitos 
-select * from obitos;
+-- Histórico de Funcionários
+SELECT
+    cf.rf as rf_funcionario,
+    cf.nome as nome_funcionario,
+    cf.setor,
+    cf.cargo,
+    cf.salario,
+    cf.turno
+FROM
+    cadastrarfuncionario cf
+JOIN
+    funcionariosConsultasTriagem fct ON cf.rf = fct.rf_funcionario;
 
--- query de login
-select rf 
-from cadastrarfuncionario
-where cpf = '12345678901'
-and data_nascimento = '29/06/2004';
+-- Histórico do Paciente
+SELECT 
+    c.id AS id_consulta,
+    p.ra AS ra_paciente,
+    p.nome AS nome_paciente,
+    c.data,
+    c.horario_consulta,
+    c.horario_consulta_inicio,
+    c.horario_consulta_termino,
+    c.situacao,
+    c.tipoconsulta,
+    c.observacoes_adicionais,
+    c.medicacoes
+FROM 
+    consultatriagem c
+JOIN 
+    cadastrarpaciente p ON c.ra_paciente = p.ra
+WHERE 
+    p.ra = 4
+    AND c.tipoconsulta LIKE '%Consulta%'
+ORDER BY 
+    c.data DESC;
 
+-- Checar Consultas/Pacientes
+SELECT 
+    c.id,
+    c.ra_paciente,
+    p.nome AS nome_paciente,
+    c.data,
+    c.horario_consulta,
+    c.horario_consulta_inicio,
+    c.horario_consulta_termino,
+    c.situacao,
+    c.tipoconsulta
+FROM 
+    consultatriagem c
+JOIN
+    cadastrarpaciente p ON c.ra_paciente = p.ra
+WHERE 
+    c.tipoconsulta LIKE '%Consulta%';
 
--- quert de recuperar senha
+-- Checar Triagem
+SELECT 
+    id, ra_paciente, data, horario_triagem, alergias, febre, tosse, nausea, vomitos, diarreia, calafrios, dorsnasarticulacoes, dificuldaderespiratoria, perdodopaladarouolfato, congestaonasal 
+FROM 
+    consultatriagem
+WHERE 
+    tipoconsulta LIKE '%triagem%';
+
+-- Consulta de Óbitos
+SELECT * FROM obitos;
+
+-- Query de Login
+SELECT rf 
+FROM cadastrarfuncionario
+WHERE cpf = '12345678901'
+AND data_nascimento = to_date('2023-12-29', 'YYYY/MM/DD');
+
+-- Query de Recuperar Senha
 SELECT rf
 FROM cadastrarfuncionario
 WHERE rg = '1234567890' 
   AND cpf = '98765432101'
-  AND data_nascimento = TO_DATE('10/02/1975', 'DD/MM/YYYY'); 
+  AND data_nascimento = to_date('2004-06-29', 'YYYY/MM/DD');
 
 
 -- Exclui as tabelas
@@ -340,5 +451,3 @@ DROP TABLE cadastrarfuncionario;
 DROP TABLE funcionariosConsultasTriagem;
 DROP TABLE obitos;
 DROP SEQUENCE auto_increment;
-
-
